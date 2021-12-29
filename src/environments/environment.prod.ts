5666fa49fe8cf4d578a6cc1c720bed42ec59f6c6
mirjalol03 <jabborovmirjalol03@gmail.com>
2021-12-29 17:57:28 +0500

    moslashub qoshimcha yangiliklar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   siteName: "Qashqadaryo Ahli",
   siteBrand: "assets/Main.logo.jpg",
-  baseUrl: 'http://api.qashqadaryoahli.uz:9090',
+  // baseUrl: 'http://api.qashqadaryoahli.uz:9090',
+  baseUrl: 'http://localhost:8080',
 };
 
